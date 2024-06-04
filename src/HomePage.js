f2dae9754633f92dea686d17b451f29274b03de7
Dibyendu <dibyendubar13702@gmail.com>
2024-06-04 17:08:29 +0530

    test-2

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,17 +6,19 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'; // Ensure you import the CSS for react-toastify
 import './HomePage.css'; // Add this to import custom CSS
 
-function HomePage({onLogout}) {
-
- //This is my code
- 
+function HomePage({ onLogout }) {
   const user = JSON.parse(localStorage.getItem('user'));
-  const jwtToken = user.credential; // Get the JWT token from the response
-  const base64Url = jwtToken.split('.')[1]; // Split the token and get the payload
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Replace characters
-  const payload = JSON.parse(window.atob(base64)); // Decode the payload
-  const userName = payload.name; // Get the user's name from the payload
-  
+
+  let userName = ''; // Default value for userName
+  if (user) {
+    const jwtToken = user.credential; // Get the JWT token from the response
+    const base64Url = jwtToken.split('.')[1]; // Split the token and get the payload
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Replace characters
+    const payload = JSON.parse(window.atob(base64)); // Decode the payload
+    userName = payload.name; // Get the user's name from the payload
+  }
+
+  console.log("We are in homepage");
   console.log('User name:', userName);
 
   return (
@@ -26,7 +28,7 @@ function HomePage({onLogout}) {
       </div>
       <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
         {user && (
-          <h2 className="greeting">Hello, {user?userName:''}!</h2>
+          <h2 className="greeting">Hello, {userName}!</h2>
         )}
         <h1>Vehicle Dock Management System</h1>
         <VehicleForm />
