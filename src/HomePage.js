d8a8f9b61426e6bb1883fbcbdbdbc9aaf5fef1f6
Dibyendu <dibyendubar13702@gmail.com>
2024-06-04 16:55:50 +0530

    prod-commit

--- src/HomePage.js
+++ src/HomePage.js
@@ -10,7 +10,13 @@ function HomePage({onLogout}) {
 
  
   const user = JSON.parse(localStorage.getItem('user'));
-  console.log("User:",user);
+  const jwtToken = user.credential; // Get the JWT token from the response
+  const base64Url = jwtToken.split('.')[1]; // Split the token and get the payload
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Replace characters
+  const payload = JSON.parse(window.atob(base64)); // Decode the payload
+  const userName = payload.name; // Get the user's name from the payload
+  
+  console.log('User name:', userName);
 
   return (
     <div className="App">
@@ -19,7 +25,7 @@ function HomePage({onLogout}) {
       </div>
       <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
         {user && (
-          <h2 className="greeting">Hello, {user?.name}!</h2>
+          <h2 className="greeting">Hello, {user?userName:''}!</h2>
         )}
         <h1>Vehicle Dock Management System</h1>
         <VehicleForm />
